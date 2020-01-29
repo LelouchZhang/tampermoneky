7c8a2250cc9ad2e3503ad0ff5e55ac9d9997f204
ZacharyZhang <33120357+LelouchZhang@users.noreply.github.com>
2020-01-29 11:18:16 +0800

    Create damai_ticket.user.js

---
+++ damai_ticket.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         damai_ticket
+// @namespace    https://github.com/LelouchZhang
+// @version      0.1
+// @description  try to take over the world!
+// @author       ZacharyZhang
+// @match        https://buy.damai.cn/orderConfirm*
+// @grant        none
+// ==/UserScript==
